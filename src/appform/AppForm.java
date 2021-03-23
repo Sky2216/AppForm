59a801486223011518253e90ca11f14cc27c9e87
Sky2216 <81198901+Sky2216@users.noreply.github.com>
2021-03-22 21:02:23 -0300

    Aula 22-03-2021 - Pacotes MVC criados e Formulario CADPESSOA

---
+++ src/appform/AppForm.java
@@ -0,0 +1,17 @@
+
+package appform;
+
+/**
+ *
+ * @author HOME
+ */
+public class AppForm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
